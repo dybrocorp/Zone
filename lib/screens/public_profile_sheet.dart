4b43d28d7d105e8edd0e4165f1b1ff357d91670f
dybrocorp <dybrocorp@gmail.com>
2026-05-03 10:00:05 -0500

    Fix PublicProfileSheet constructor parameter missing userId

--- lib/screens/public_profile_sheet.dart
+++ lib/screens/public_profile_sheet.dart
@@ -14,6 +14,7 @@ class PublicProfileSheet extends StatefulWidget {
 
   const PublicProfileSheet({
     super.key,
+    required this.userId,
     this.realUid,
     required this.userName,
     this.instagramHandle,
